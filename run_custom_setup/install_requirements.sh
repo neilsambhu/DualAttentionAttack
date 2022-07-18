e1fb6f492d68fdd856a431c4fd0475307e9faedc
Neil Sambhu <neilsambhu@gmail.com>
2022-07-18 14:52:55 -0400

    opencv install

--- run_custom_setup/install_requirements.sh
+++ run_custom_setup/install_requirements.sh
@@ -4,4 +4,5 @@
 # https://pytorch.org/get-started/previous-versions/#v140
 conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
 pip install neural-renderer-pytorch==1.1.3
-conda install -c conda-forge tqdm opencv
+conda install -c conda-forge tqdm
+pip install opencv-python
